61a6cbd7e1771c6141508a404b11f216b59ad27b
valfaro123 <valfaro6@horizon.csueastbay.edu>
2021-12-19 21:56:54 -0800

    create about component

---
+++ src/components/About.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function About() {
+    return (
+        <div className="container mt-64 flex justify-between items-center mx-auto px-8 md:px-14">
+            <section className="w-full">
+                <h2 id="about" className="secondary-title">About Me</h2>
+                <p className="section-paragraph">
+                ontrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.
+                </p>
+            </section>
+        </div>
+    )
+}
