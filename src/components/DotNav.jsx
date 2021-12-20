bd0ecc6edf09dec2021f17271771701dd221d5dd
valfaro123 <valfaro6@horizon.csueastbay.edu>
2021-12-19 21:57:51 -0800

    add functionality to dotnav

--- src/components/DotNav.jsx
+++ src/components/DotNav.jsx
@@ -1,22 +1,78 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+
+
 
 export default function DotNav() {
+    const [selected, setSelected] = useState("Home");
+
+    useEffect(() => {
+        if (typeof window !== "undefined") {
+          window.addEventListener("scroll", () => {
+            let currentScrollPosition = window.pageYOffset;
+
+
+              switch( true ){
+                case  window.pageYOffset >= 1800:
+                    { selected !== "Contact" && handleSelect("Contact") }
+                    break;
+                
+                case  (currentScrollPosition >= 1300):
+                    { selected !== "Projects" && handleSelect("Projects")}
+                    break;
+                
+                case (currentScrollPosition >= 700):
+                    { selected !== "About" && handleSelect("About")}
+                    break;
+                case (currentScrollPosition < 600):
+                    { selected !== "Home" && handleSelect("Home")}
+                        break;
+
+              }
+          }
+            
+          );
+        }
+      }, []);
+    const dotNavItems = [
+        {
+            href: "#",
+            name: "Home",
+
+        },
+        {
+            href: "#about",
+            name: "About",
+        },
+        {
+            href: "#projects",
+            name: "Projects",
+        },
+        {
+            href: "#contact",
+            name: "Contact",
+        },
+    ];
+
+    const handleSelect = (itemName) => {
+        console.log("rerendered")
+        setSelected(itemName);
+    }
+    
     return (
+        
         <div className="dotnav">
-            <nav className="inline-block lg:mr-24 lg:w-4 fixed left-percentage hidden xl:block">
+            <nav className="inline-block fixed left-percentage hidden xl:block">
+
                 <div className="absolute left-50 transform -translate-x-1/2 space-y-6 mt-36">
-                    <a href="#" className="nav-dot selected-circle block w-7 h-7 rounded-full border-4 border-nav bg-body">
-                        <span className="bg-black px-2 py-1 rounded-md ml-10">Home</span>
-                    </a>
-                    <a href="#work" className="nav-dot block w-7 h-7 rounded-full border-4 border-nav bg-body">
-                        <span className="bg-black px-2 py-1 rounded-md ml-10 opacity-0">Work</span>
-                    </a>
-                    <a href="#clients" className="nav-dot block w-7 h-7 rounded-full border-4 border-nav bg-body">
-                        <span className="bg-black px-2 py-1 rounded-md ml-10 opacity-0">Clients</span>
-                    </a>
-                    <a href="#hire" className="nav-dot block w-7 h-7 rounded-full border-4 border-nav bg-body">
-                        <span className="bg-black px-2 py-1 rounded-md ml-10 opacity-0">Hire</span>
-                    </a>
+                    {
+                        dotNavItems.map( item => {
+                            return( 
+                                <a href={item.href} className={ item.name === selected ? "selected-circle" : "deselected-circle"}>
+                                    <span className="bg-black px-2 py-1 rounded-md ml-10 opacity-0">{item.name}</span>
+                                </a>
+                            )
+                        })
+                    }
                 </div>
               </nav>
         </div>
