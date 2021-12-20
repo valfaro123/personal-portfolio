259ef9947cd8fa10d949ba09917274644b2a22f4
valfaro123 <valfaro6@horizon.csueastbay.edu>
2021-12-19 21:56:17 -0800

    add components to app

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 
 import './App.css';
+import About from './components/About';
+import Contact from './components/Contact';
+import Header from './components/Header';
+import Navbar from './components/Navbar';
+import Projects from './components/Projects';
 
 function App() {
   return (
-    <div className="App">
-      <h1 className="text-4xl font-bold underline">
-        Hello world!
-      </h1>
+    <div className="bg-body text-white font-poppins pb-12">
+      <Navbar/>
+      <Header/>
+      <About/>
+      <Projects/>
+      <Contact/>
     </div>
   );
 }
