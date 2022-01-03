4d69b8f3a084a29d157dbea0bdc433ecdcb5b5c7
valfaro123 <valfaro6@horizon.csueastbay.edu>
2022-01-02 21:39:28 -0800

    add single project component

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,15 +1,43 @@
 import React from 'react'
 
 export default function Projects() {
+    const proj = [ 
+        {
+            img: "https://images.pexels.com/photos/1029757/pexels-photo-1029757.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=800",
+            title: "proj1",
+            desc: "blah blah blahhhh hhhhh blah blah blah hhhhhhh hhhhh hhhhhhhhh hhhh hhhhhhh hhhhhh hhhhh hhhh hhhhhhhh hhh",
+            repoLink: "https://github.com/",
+            siteLink: "https://www.google.com/",
+        },
+        {
+            img: "https://images.pexels.com/photos/1779487/pexels-photo-1779487.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=800",
+            title: "proj2",
+            desc: "blah blah blahhhhhhhhh blah blah blahhhhhhhhh",
+            repoLink: "https://github.com/",
+            siteLink: "https://www.google.com/",
+        },
+        {
+            img: "https://images.pexels.com/photos/38568/apple-imac-ipad-workplace-38568.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=800",
+            title: "proj2",
+            desc: "blah blah blahhhhhhhhh blah blah blahhhhhhhhh",
+            repoLink: "https://github.com/",
+            siteLink: "https://www.google.com/",
+        }
+    ]
     return (
-        <div className="container mt-64 flex justify-between items-center mx-auto px-9 md:px-14">
-            <section className="w-full">
+        <div className="container mt-96 flex justify-between items-center mx-auto px-9">
+            <section className="">
                 <h2 id="projects" className="secondary-title">Featured Projects</h2>
                 
                 <div className="grid  grid-cols-1  lg:grid-cols-3 gap-10 mt-6">
-                    <img src="https://cdn.pixabay.com/photo/2021/05/24/07/10/sti-6278035__340.jpg"/>
-                    <img src="https://cdn.pixabay.com/photo/2021/05/24/07/10/sti-6278035__340.jpg"/>
-                    <img src="https://cdn.pixabay.com/photo/2021/05/24/07/10/sti-6278035__340.jpg"/>
+                    {proj.map( (p, index) => {
+                        return(
+                            <div key={index}>
+                                
+                                
+                            </div>
+                        )
+                    })}
                 </div>
             </section>
         </div>
