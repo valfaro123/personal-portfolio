fd1a373b8af138c84e9ced724b769dceac37ce4b
valfaro123 <valfaro6@horizon.csueastbay.edu>
2022-01-24 18:01:45 -0800

    edit page content

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,7 +7,7 @@ export default function Header() {
             <div className="flex flex-wrap md:flex-nowrap">
                 <DotNav/>
                 <div className="flex flex-wrap lg:ml-20 justify-center md:justify-start max-w-xl mt-0 md:my-36">
-                    <h1 className="font-bold text-5xl md:text-6xl lg:text-7xl text-center md:text-left">Front End<br/>  web developer.</h1>
+                    <h1 className="font-bold text-5xl  text-center md:text-left"> Hi, I'm Victor<br/> and I'm a Front-End Develooper  </h1>
                     <div className="w-full flex justify-center md:justify-start">
                         <a href="#about" className="px-8 py-4 bg-theme text-white font-bold mt-12 flex items-center space-x-3">
                             <span>Learn More</span>
