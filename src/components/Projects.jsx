7ddc028dc28bb4e11c57b67fad0fdd4db4697926
valfaro123 <valfaro6@horizon.csueastbay.edu>
2022-01-24 16:59:18 -0800

    add content for projects

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,27 +1,31 @@
 import React from 'react'
+import SingleProject from './SingleProject'
+import proj1 from '../images/ableton-mockup.jpg'
+import proj2 from '../images/novacane-mockup.jpg'
+import proj3 from '../images/crypto-mockup.jpg'
 
 export default function Projects() {
     const proj = [ 
         {
-            img: "https://images.pexels.com/photos/1029757/pexels-photo-1029757.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=800",
-            title: "proj1",
-            desc: "blah blah blahhhh hhhhh blah blah blah hhhhhhh hhhhh hhhhhhhhh hhhh hhhhhhh hhhhhh hhhhh hhhh hhhhhhhh hhh",
-            repoLink: "https://github.com/",
-            siteLink: "https://www.google.com/",
+            img: proj1,
+            title: "Ableton About Page",
+            desc: "Having struggled with styling, I decided to focus on practicing CSS and Tailwind for this project. Using only screenshots from the live page, I built a responsive clone of the landing page with ReactJS and styled using Tailwind and some custom CSS.",
+            repoLink: "https://github.com/valfaro123/ableton-about-clone",
+            siteLink: "https://valfaro123.github.io/ableton-about-clone/",
         },
         {
-            img: "https://images.pexels.com/photos/1779487/pexels-photo-1779487.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=800",
-            title: "proj2",
-            desc: "blah blah blahhhhhhhhh blah blah blahhhhhhhhh",
-            repoLink: "https://github.com/",
-            siteLink: "https://www.google.com/",
+            img: proj2,
+            title: "Artist Website Novacane",
+            desc: "I designed and built a simple ReactJS content hub for a musician that helped me gain experience with launching sites to custom domains as well as breaking them and learning to find a solution.",
+            repoLink: "https://github.com/valfaro123/novacane",
+            siteLink: "https://www.novacane.co/",
         },
         {
-            img: "https://images.pexels.com/photos/38568/apple-imac-ipad-workplace-38568.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=800",
-            title: "proj2",
-            desc: "blah blah blahhhhhhhhh blah blah blahhhhhhhhh",
-            repoLink: "https://github.com/",
-            siteLink: "https://www.google.com/",
+            img: proj3,
+            title: "Crypto Dashboard",
+            desc: "Using a cryptocurrency and news API from rapidAPI, I built a dashboard which featured live prices, news, and information on the top 100 currencies. The project was built using ReactJS and uses components from ANT Designs UI library.",
+            repoLink: "https://github.com/valfaro123/crypto-base",
+            siteLink: "https://valfaro123.github.io/crypto-base/",
         }
     ]
     return (
@@ -33,7 +37,13 @@ export default function Projects() {
                     {proj.map( (p, index) => {
                         return(
                             <div key={index}>
-                                
+                                <SingleProject 
+                                    img={p.img}
+                                    title={p.title}
+                                    desc={p.desc}
+                                    repoLink={p.repoLink}
+                                    siteLink={p.siteLink}
+                                />
                                 
                             </div>
                         )
